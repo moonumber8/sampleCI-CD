27d1e8539b1df5dba7317ff2e1e8e189ce3bdd5c
moonumber8 <moo-hesoyam@hotmail.com>
2025-04-12 00:01:04 +0700

    23434

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("9999999999999");
+		System.out.println("ตาอ้วนมั้มๆ");
 	}
 
 }
